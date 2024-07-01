f6dd60122f7052fc1bd9c23eff2d1d59870d0b50
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-07-01 18:24:05 +0530

    Catoegory updating in the swiggy website

--- swiggy-clone/src/App.jsx
+++ swiggy-clone/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Category from './components/Category';
 // import "./App.css"
 
 function App() {
   return (
     <>
     <Header/>
+    <Category/>
     </>
   );
 }
---
+++ swiggy-clone/src/components/Category.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Category() {
+  return (
+    <div className="max-w-[1200px] mx-auto ">
+      <div className="my-2 flex item-center justify-between">
+        <div className="text-[22px] font-bold">What's on your mind?</div>
+        <div className="flex">
+          <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2"></div>
+          <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
