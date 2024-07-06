8f8d7eb239f1fe42a92995cf19a7653cea183e67
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-07-06 23:47:38 +0530

    Project started

--- swiggy-clone/src/components/Category.jsx
+++ swiggy-clone/src/components/Category.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { HiArrowSmLeft } from "react-icons/hi";
+import { HiArrowSmRight } from "react-icons/hi";
 
 function Category() {
   return (
@@ -6,8 +8,12 @@ function Category() {
       <div className="my-2 flex item-center justify-between">
         <div className="text-[22px] font-bold">What's on your mind?</div>
         <div className="flex">
-          <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2"></div>
-          <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2"></div>
+          <div className="cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
+            <HiArrowSmLeft />
+          </div>
+          <div className="cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
+            <HiArrowSmRight />
+          </div>
         </div>
       </div>
     </div>
